639fec3f76cd9cc9e32e31aac9bfa9e1d6ca5a73
EricNiyo73 <ericniyokwizerwa1@gmail.com>
2024-03-21 09:57:16 +0200

    todos middleware

--- src/Routes/todoRoutes.ts
+++ src/Routes/todoRoutes.ts
@@ -7,8 +7,8 @@ const router = Router();
 router.post("/create", TodoController.createTodo);
 router.get("/", TodoController.findAllTodo);
 router.get("/:id", TodoController.findOneTodo);
-router.put("/:id", authentication, TodoController.updateTodo);
-router.delete("/:id", authentication, TodoController.deleteT);
+router.put("/:id", TodoController.updateTodo);
+router.delete("/:id", TodoController.deleteT);
 // router.delete("/many", TodoController.deleteMany);
 
 export default router;
